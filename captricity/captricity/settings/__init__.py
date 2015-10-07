f330904e19513b1c04f162f78164b540371a4442
Issac Kelly <issac@issackelly.com>
2015-10-06 21:24:19 -0700

    No hard failure on without local settings.

--- captricity/captricity/settings/__init__.py
+++ captricity/captricity/settings/__init__.py
@@ -4,6 +4,7 @@ from .base import *
 try:
     from .local import *
 except ImportError as exc:
-    exc.args = tuple(
-        ['%s (did you rename settings/local-dist.py?)' % exc.args[0]])
-    raise exc
+    print "No local settings found"
+    #exc.args = tuple(
+    #    ['%s (did you rename settings/local-dist.py?)' % exc.args[0]])
+    #raise exc
