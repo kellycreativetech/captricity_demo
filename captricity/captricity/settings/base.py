6d9bfbc2ac8fbc01962f5b30f48797b86378cfdf
Issac Kelly <issac@issackelly.com>
2015-10-06 21:20:19 -0700

    Add Heroku DB stuff.

--- captricity/captricity/settings/base.py
+++ captricity/captricity/settings/base.py
@@ -207,12 +207,35 @@ FILE_UPLOAD_PERMISSIONS = 0o0664
 WSGI_APPLICATION = 'captricity.wsgi.application'
 
 # Define your database connections
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'captricity_demo',
+
+# Heroku settings
+import urlparse
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+try:
+    if os.environ.has_key('DATABASE_URL'):
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        DATABASES = {}
+        DATABASES['default'] = {
+            'NAME':     url.path[1:],
+            'USER':     url.username,
+            'PASSWORD': url.password,
+            'HOST':     url.hostname,
+            'PORT':     url.port,
+        }
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'captricity_demo',
+        }
     }
-}
+
+
 
 # Recipients of traceback emails and other notifications.
 ADMINS = (
